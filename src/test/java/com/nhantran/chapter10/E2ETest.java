c1a02c7c70df9f9ab653044783bb4986761c5920
Nhan Brown <ttnhan2642@gmail.com>
2024-06-26 14:33:57 +0700

    create e2e test

---
+++ src/test/java/com/nhantran/chapter10/E2ETest.java
@@ -0,0 +1,63 @@
+package com.nhantran.chapter10;
+
+import com.nhantran.base.TestBase;
+import com.nhantran.common.Constants;
+import com.nhantran.common.Messages;
+import com.nhantran.enums.BookTicketComboBoxes;
+import com.nhantran.enums.RailwayStations;
+import com.nhantran.enums.RailwayTabs;
+import com.nhantran.enums.SeatTypes;
+import com.nhantran.models.Tickets;
+import com.nhantran.models.User;
+import com.nhantran.pages.*;
+import com.nhantran.utils.controls.WindowControl;
+import com.nhantran.utils.helpers.DateTimeHelper;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class E2ETest extends TestBase {
+
+    private HomePage homePage = new HomePage();
+    private RegisterPage registerPage = new RegisterPage();
+    private LoginPage loginPage = new LoginPage();
+    private MailboxPage mailboxPage = new MailboxPage();
+    private RegistrationConfirmationPage registrationConfirmationPage = new RegistrationConfirmationPage();
+    private TimetablePage timetablePage = new TimetablePage();
+    private BookTicketPage bookTicketPage = new BookTicketPage();
+    private BookTicketSuccessPage bookTicketSuccessPage = new BookTicketSuccessPage();
+    private MyTicketPage myTicketPage = new MyTicketPage();
+
+    private User newUser = new User(null, "1234567890", "1234567890", "11111111111");
+    private Tickets ticket = new Tickets(DateTimeHelper.calculateNextDate(10), RailwayStations.DA_NANG, RailwayStations.HUE, SeatTypes.HARD_SEAT, 1);
+
+    @Test(description = "Validate the flow from start to finish, simulate real behavior of user")
+    public void EntToEndTest() {
+        homePage.clickCreateAccountHyperlink();
+        String railwayWindow = WindowControl.getWindowHandle();
+        WindowControl.openSiteInNewTab(Constants.TEMPORARY_MAIL_URL);
+        mailboxPage.uncheckScrambleAddressCheckbox();
+        newUser.setEmail(mailboxPage.getMail());
+        String mailWindow = WindowControl.getWindowHandle();
+        WindowControl.switchToWindow(railwayWindow);
+        registerPage.register(newUser);
+        WindowControl.switchToWindow(mailWindow);
+        WindowControl.refresh();
+        mailboxPage.clickConfirmLinkInMail();
+        WindowControl.switchToRemainingTab(railwayWindow, mailWindow);
+        Assert.assertEquals(registrationConfirmationPage.getConfirmationSuccessMessage(), Messages.MSG_SUCCESS_CONFIRM_REGISTRATION, "The registration confirmation message does not match");
+        registrationConfirmationPage.clickTab(RailwayTabs.LOGIN);
+        loginPage.login(new User(newUser.getEmail(), newUser.getPassword()));
+        homePage.clickTab(RailwayTabs.TIMETABLE);
+        timetablePage.clickBookTicketLink(ticket.getDepartStation(), ticket.getArrivalStation());
+        Assert.assertEquals(bookTicketPage.getStation(BookTicketComboBoxes.DEPART_STATION), ticket.getDepartStation().getValue(), "Depart station does not match");
+        Assert.assertEquals(bookTicketPage.getStation(BookTicketComboBoxes.ARRIVE_STATION), ticket.getArrivalStation().getValue(), "Arrive station does not match");
+        bookTicketPage.bookTicket(ticket);
+        Assert.assertTrue(bookTicketSuccessPage.isSuccessfulMessageDisplayed("Ticket booked successfully!"), "Success message does not show");
+        bookTicketSuccessPage.clickTab(RailwayTabs.MY_TICKET);
+        myTicketPage.cancelTicket(ticket);
+        myTicketPage.acceptToCancelTicket();
+        myTicketPage.clickTab(RailwayTabs.LOG_OUT);
+        Assert.assertTrue(homePage.isHomePageTitleDisplayed());
+        Assert.assertFalse(homePage.isTabDisplayed(RailwayTabs.LOG_OUT));
+    }
+}
