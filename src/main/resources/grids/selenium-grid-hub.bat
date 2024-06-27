96eaef40494560ae8421eda0844771999ac62d2e
Nhan Brown <ttnhan2642@gmail.com>
2024-06-27 11:48:49 +0700

    create file .bat to run grid node and hub

---
+++ src/main/resources/grids/selenium-grid-hub.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Set the path to the Selenium server jar file
+SET SELENIUM_SERVER_JAR=selenium-server-4.21.0.jar
+
+REM Check if the Selenium server jar file exists
+IF NOT EXIST %SELENIUM_SERVER_JAR% (
+    echo Selenium server jar file not found!
+    exit /b 1
+)
+
+REM Run the Selenium server
+echo Starting Selenium server...
+java -jar %SELENIUM_SERVER_JAR% hub
